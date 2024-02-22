2905be6169998adb84ae7c8e44005b323be28a3a
tashiro <andre_seichi@hotmail.com>
2024-02-22 19:25:40 -0300

    feat: util site config

---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "Dog Voting",
+  description:
+    "Create and vote on polls for your public or private group. Share the polls with your friends and family.",
+  links: {
+    twitter: "https://twitter.com/andre_seichi",
+    github: "https://github.com/andreseichi",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
