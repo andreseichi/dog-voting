739bc0b88b9b9d0b307f567112e79aedcfc8e453
tashiro <andre_seichi@hotmail.com>
2024-02-25 18:01:22 -0300

    refactor: removed button for outline

--- src/app/components/Header/HeaderNav.tsx
+++ src/app/components/Header/HeaderNav.tsx
@@ -5,22 +5,21 @@ import { BarChart, Vote } from "lucide-react";
 
 export function HeaderNav() {
   return (
-    <nav className="flex">
-      <Link href="/polls" className="flex items-center justify-center">
-        <Button
-          variant={"link"}
-          className="items-center justify-center space-x-1"
-        >
-          <Vote size={16} />
-          <span>Polls</span>
-        </Button>
+    <nav className="flex space-x-4">
+      <Link
+        href="/polls"
+        className="flex items-center justify-center space-x-1 rounded-md p-1 text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+      >
+        <Vote size={16} />
+        <span>Polls</span>
       </Link>
 
-      <Link href="/new-poll" className="flex items-center justify-center">
-        <Button variant={"link"} className="space-x-1">
-          <BarChart size={16} />
-          <span>Results</span>
-        </Button>
+      <Link
+        href="/results"
+        className="flex items-center justify-center space-x-1 rounded-md p-1 text-primary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+      >
+        <BarChart size={16} />
+        <span>Results</span>
       </Link>
     </nav>
   );
