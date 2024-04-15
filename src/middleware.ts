b5be787e0ce543133674389a06d6dfa0aed0aeb5
tashiro <andre_seichi@hotmail.com>
2024-04-14 21:10:41 -0300

    feat: middleware

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { withAuth } from "next-auth/middleware";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+import { getToken } from "next-auth/jwt";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest, response: NextResponse) {
+  const token = await getToken({ req: request });
+
+  console.log({ token });
+
+  if (token) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+// export default withAuth(function middleware(req) {
+//   console.log(req.nextauth);
+// });
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/my-polls"],
+};
