f51be07af64fb3cdf4b3e0c2c2ffadec2ef761d3
André Tashiro <andre_seichi@hotmail.com>
2024-10-20 16:50:55 -0300

    feat: loading for sign in

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -7,9 +7,34 @@ import { NewPoll } from "@/app/components/NewPoll";
 import { UserNav } from "@/app/components/UserNav";
 import { Button } from "@/components/ui/button";
 import { HeaderNav } from "./HeaderNav";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
 export function Header() {
+  const [isLoadingSignIn, setIsLoadingSignIn] = useState(false);
   const session = useSession();
+
+  const { toast } = useToast();
+
+  const handleSignIn = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    console.log("hey");
+    e.preventDefault();
+    try {
+      setIsLoadingSignIn(true);
+      await signIn("google");
+      setIsLoadingSignIn(false);
+    } catch (error) {
+      setIsLoadingSignIn(false);
+      toast({
+        title: "Ocorreu um erro",
+        description: "Por favor, tente novamente mais tarde",
+        duration: 5000,
+        variant: "destructive",
+      });
+    }
+  };
 
   return (
     <div className="sticky left-0 right-0 top-0 z-50 border-b bg-background">
@@ -29,8 +54,31 @@ export function Header() {
           {session.data ? (
             <UserNav />
           ) : (
-            <Button variant={"secondary"} onClick={() => signIn("google")}>
+            <Button variant={"secondary"} onClick={handleSignIn}>
               Sign In
+              {isLoadingSignIn && (
+                <svg
+                  className="ml-3 h-5 w-5 animate-spin text-white"
+                  xmlns="http://www.w3.org/
+                  2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V2.5"
+                  ></path>
+                </svg>
+              )}
             </Button>
           )}
         </div>
